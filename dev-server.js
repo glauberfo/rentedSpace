d2419be7e2686f8654d26ba36724d8e1409481e0
Glauber Oliveira <glauber.oliveira@platformbuilders.io>
2025-11-13 19:30:24 -0300

    Ajustes

---
+++ dev-server.js
@@ -0,0 +1,10 @@
+// Placeholder dev server for Builder.io
+console.log('Flutter project - dev server placeholder');
+console.log('This is a Flutter project. Use: flutter run');
+console.log('Keeping process alive for Builder.io...');
+
+// Keep process alive
+setInterval(() => {
+  // Do nothing, just keep running
+}, 60000);
+
