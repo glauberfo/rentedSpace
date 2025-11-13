140eb808009b44a62f54c91affde7f14bc40429d
Glauber Oliveira <glauber.oliveira@platformbuilders.io>
2025-11-13 19:37:22 -0300

    Add server

--- dev-server.js
+++ dev-server.js
@@ -1,10 +1,28 @@
 // Placeholder dev server for Builder.io
-console.log('Flutter project - dev server placeholder');
-console.log('This is a Flutter project. Use: flutter run');
-console.log('Keeping process alive for Builder.io...');
+const http = require('http');
 
-// Keep process alive
-setInterval(() => {
-  // Do nothing, just keep running
-}, 60000);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Flutter Project - Builder.io Placeholder</title>
+    </head>
+    <body>
+      <h1>Flutter Project</h1>
+      <p>This is a Flutter project. Use: flutter run</p>
+      <p>This server is only for Builder.io integration.</p>
+    </body>
+    </html>
+  `);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Flutter project - dev server placeholder`);
+  console.log(`This is a Flutter project. Use: flutter run`);
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log('Keeping process alive for Builder.io...');
+});
 
